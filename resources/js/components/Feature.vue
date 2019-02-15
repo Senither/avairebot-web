42d1902432fd0c4d5930ea9f87c4f5652c1ba31c
Alexis Tan <alexis@sen-dev.com>
2019-02-15 12:47:13 +0100

    Create feature list for the home page

---
+++ resources/js/components/Feature.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="column">
+        <a v-if="this.hasIcon" :href="getUrl()">
+            <i class="fas is-primary" :class="icon"></i>
+        </a>
+
+        <a :href="getUrl()">
+            <h4>{{ title }}</h4>
+            <p>{{ body }}</p>
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: String,
+            body: String,
+            icon: String,
+            to: String,
+        },
+        methods: {
+            hasIcon() {
+                return this.icon != undefined;
+            },
+            getUrl() {
+                if (this.to == undefined) {
+                    return '#';
+                }
+                return this.url(this.to);
+            }
+        }
+    }
+</script>
+
+<style type="text/css" scoped>
+    i {
+        font-size: 2.5em;
+        padding-bottom: 1.85vh;
+    }
+</style>
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,25 +1,100 @@
 <template>
     <section class="section content">
         <div class="container">
-            <h1 class="title">Home</h1>
-            <h2 class="subtitle">
-                Lorem ipsum dolor sit amet.
-            </h2>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div class="columns">
+                <div class="column has-text-centered">
+                    <h2 class="title is-2 is-spaced">Discord bot features</h2>
+                    <h5 class="subtitle is-5">Equip your Discord server with some of our Discord bot's most popular features.</h5>
+
+                    <br>
+                </div>
+            </div>
+
+            <div class="columns has-text-centered">
+                <feature
+                    to="#/docs"
+                    icon="fa-music"
+                    title="Stream Music"
+                    body="Request music via youtube, soundcloud, twitch, radio links, or by name with ease."
+                />
+                <feature
+                    to="#/docs"
+                    icon="fa-list"
+                    title="Music Playlists"
+                    body="Create custom music playlists and share your playlists with other people with ease."
+                />
+                <feature
+                    to="#/docs"
+                    icon="fa-gavel"
+                    title="Administration"
+                    body="Administration made easy, take a load off your shoulders and let Ava do the hard work."
+                />
+                <feature
+                    to="#/docs"
+                    icon="fa-trophy"
+                    title="Level & Leaderboards"
+                    body="Earn XP and levels, and ranks through level ups, show off your rank via the website leaderboard on in-discord."
+                />
+            </div>
+
+            <div class="columns has-text-centered">
+                <div class="column">
+                    <feature
+                        to="#/docs"
+                        icon="fa-hand-holding-heart"
+                        title="Join & Leave Messages"
+                        body="Broadcast messages when users joins or leaves your server on a per-channel basis."
+                    />
+                </div>
+                <div class="column">
+                    <feature
+                        to="#/docs"
+                        icon="fa-globe"
+                        title="Bilingual Commands"
+                        body="Ava supports multiple languages, you can change what languages Ava uses on the fly."
+                    />
+                </div>
+                <div class="column">
+                    <feature
+                        to="#/docs"
+                        icon="fa-pencil-alt"
+                        title="Customizable Prefixes"
+                        body="Don't like the default prefix? No worries, you can change it to anything you'd like."
+                    />
+                </div>
+                <div class="column">
+                    <feature
+                        to="#/docs"
+                        icon="fa-gamepad"
+                        title="Lots of Fun Commands"
+                        body="Ava has a lot of fun/game commands, and more are constantly being added"
+                    />
+                </div>
+            </div>
+
+            <div class="columns">
+                <div class="column has-text-centered">
+                    <br>
+
+                    <h5 class="subtitle is-5">And much more!</h5>
+                    <p>Learn more by checking out the <a href="#/docs">Command List</a> to see all the commands in Ava.</p>
+                </div>
+            </div>
+
         </div>
-  </section>
+    </section>
 </template>
 
 <script>
+    import Feature from '../components/Feature';
+
     export default {
         mounted() {
             console.log('Home mounted.');
+        },
+        components: {
+            Feature
         }
     }
 </script>
