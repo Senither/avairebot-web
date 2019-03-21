e7aade493d171771b5653a24562f52d5616bfe92
Alexis Tan <alexis@sen-dev.com>
2019-03-21 21:19:08 +0100

    Fix loading animation for the commands view

--- resources/js/views/Commands.vue
+++ resources/js/views/Commands.vue
@@ -3,7 +3,7 @@
         <div class="section container">
             <h1 class="title is-1">Command List</h1>
 
-            <p>Below you'll find a list of all <strong>{{ totalCommands }}</strong> commands that Ava has, along with a short description of what each command does.</p>
+            <p>Below you'll find a list of all <strong>{{ totalCommands == 0 ? '???' : totalCommands }}</strong> commands that Ava has, along with a short description of what each command does.</p>
 
             <h5 class="subtitle is-5">Command Usage Syntax</h5>
             <ul>
@@ -12,34 +12,36 @@
             </ul>
         </div>
 
-        <div class="container">
-            <div class="tabs is-fullwidth">
-                <ul>
-                    <li :class="{ 'is-active': this.selected == null }">
-                        <a v-on:click="setSelectedCategory(null)">
-                            <span>All</span>
-                        </a>
-                    </li>
-                    <li v-for="category in Object.keys(this.commands)" :class="{ 'is-active': selected == category }">
-                        <a v-on:click="setSelectedCategory(category)">
-                            <span>{{ category }}</span>
-                        </a>
-                    </li>
-                </ul>
+        <div v-if="this.commands != null">
+            <div class="container">
+                <div class="tabs is-fullwidth">
+                    <ul>
+                        <li :class="{ 'is-active': this.selected == null }">
+                            <a v-on:click="setSelectedCategory(null)">
+                                <span>All</span>
+                            </a>
+                        </li>
+                        <li v-for="category in Object.keys(this.commands)" :class="{ 'is-active': selected == category }">
+                            <a v-on:click="setSelectedCategory(category)">
+                                <span>{{ category }}</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
 
-        <div class="section container" v-if="this.totalCommands > 0" >
-            <div v-for="category in Object.keys(this.sortedCommands)">
-                <command-category
-                    :name="category"
-                    :category="sortedCommands[category]"
-                />
+            <div class="section container" v-if="this.totalCommands > 0" >
+                <div v-for="category in Object.keys(this.sortedCommands)">
+                    <command-category
+                        :name="category"
+                        :category="sortedCommands[category]"
+                    />
+                </div>
             </div>
         </div>
 
         <div class="section container has-text-centered" v-else>
-            <h3 class="title is-3">Loading...</h3>
+            <p class="button is-loading transparent"></p>
         </div>
   </section>
 </template>
