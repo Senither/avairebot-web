be2ce057a1a2a91fb52456d80e206fae0ac7bdeb
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-04-25 21:06:42 +0200

    Update buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,5 +1,9 @@
 # This is buildpack-run.sh
 # Remove package-lock as this is already done 
+npm install --global cross-env
+
+yarn prod
+
 rm -rf package-lock.json
 
 composer install
@@ -7,7 +11,3 @@ composer install
 php artisan key:generate
 
 php artisan migrate --seed
-
-npm install --global cross-env
-
-yarn prod
