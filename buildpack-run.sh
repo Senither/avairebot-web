e797ac4723f3f0f31b87195386e4e0221ba98618
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-04-25 21:43:38 +0200

    Update buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,9 +1,9 @@
 # This is buildpack-run.sh
-# Remove package-lock as this is already done 
+# Remove package-lock as this is already done, Heroku can't decide at the node stage that yarn.local should be used instead.
 rm -rf package-lock.json
-
+# Installing the depedecies after PHP has been installed
 composer install
-
+# Generate artisan key
 php artisan key:generate
-
+# Migrate seed
 php artisan migrate --seed
