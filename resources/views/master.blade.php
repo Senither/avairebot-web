2b6c800d23716e8802e6d1c83a867f66fc80a7db
Alexis Tan <alexis@sen-dev.com>
2019-07-02 23:44:44 +0200

    Remove full URLs when loading local resources

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Loading...</title>
-    <link rel="stylesheet" href="{{ url('/') . mix('/css/avaire.css') }}">
+    <link rel="stylesheet" href="{{ mix('/css/avaire.css') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
 <body>
@@ -14,7 +14,7 @@
     <script>
         window.baseUrl = "{{ trim(config('app.url'), '/') }}";
     </script>
-    <script src="{{ url('/') . mix('/js/avaire.js') }}"></script>
+    <script src="{{ mix('/js/avaire.js') }}"></script>
     <script type="text/javascript" id="cookieinfo"
         src="//cookieinfoscript.com/js/cookieinfo.min.js"
         data-fg="#EBEBEB"
